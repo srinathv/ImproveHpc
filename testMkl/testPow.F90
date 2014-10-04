981f9ce25e4bf265577c923d73ca94eda0d7d41b
Srinath Vadlamani <srinath@txcorp.com>
2014-10-04 12:18:50 -0400

    trying to use system clock

--- testMkl/testPow.F90
+++ testMkl/testPow.F90
@@ -8,12 +8,13 @@ integer, parameter :: k = 10000
 integer,dimension(k) :: a=0,b=0,c=0,d=0
 integer :: i
 real:: t1=0,t2=0,t3=0,t4=0,t5=0,t6=0
+real :: clock_rate, clock_max
 
-call cpu_time(t1)
+call system_clock ( t1, clock_rate, clock_max )
 do i = 1, k
   c(i)=a(i)**b(i)
 end do
-call cpu_time(t2)
+call system_clock ( t2, clock_rate, clock_max )
 
 #ifdef MKL
 call cpu_time(t3)
@@ -21,7 +22,7 @@ call vdpow(k,a,b,d)
 call cpu_time(t4)
 #endif
 
-write(*,*) " ** c(1) = ",c(1), " time = ",  t2-t1
+write(*,*) " ** c(1) = ",c(1), " time = ",real ( t2 - t1 ) / real ( clock_rate )
 write(*,*) " vdpow d(1) = ",d(1), "time = ", t4-t3
 
 
