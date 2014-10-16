49501c0270efe64251b3a5658f83c1d1565f2106
Srinath Vadlamani <srinathv@ucar.edu>
2014-10-15 22:46:27 -0600

    forgot the source file added AVX

--- openMp/testSIMD/makeAndRun.py
+++ openMp/testSIMD/makeAndRun.py
@@ -38,7 +38,7 @@ def shellCommand(command,errorMessage):
   return
 
 
-ifdefsList=['SIMD','SIMDU','SIMDUL','SIMDL','SIMDA','SIMDAU','SIMDAL','SIMDAUL','NONE']
+ifdefsList=['SIMD','SIMDU','SIMDUL','SIMDL','SIMDA','SIMDAU','SIMDAL','SIMDAUL','NONE','AVX']
 filesList=['init','sum','main']
 
 def main(argv):
@@ -60,20 +60,25 @@ def main(argv):
     if ifdef == 'NONE':
        ifdefMacro = ' '
        cmdBase='ifort'
+    elif ifdef == 'AVX':
+       ifdefMacro = ' '
+       cmdBase='ifort -xAVX'
+
     else:
        ifdefMacro = '-D' + ifdef
        cmdBase='ifort -openmp'
 
     objFileList=[]
+    exe=''
 
     for fileBase in filesList:
       
       objFile=fileBase + '_'+ ifdef + '.o'
       objFileList.append(objFile)
-      command1 = cmdBase + ' ' + ifdefMacro + ' -o ' + objFile
+      command1 = cmdBase + ' ' + ifdefMacro + ' ' + fileBase+'.f90' + ' -o ' + objFile
       errorMessage = ' compilation failed for ' + objFile
-      shellCommand(command1,errorMessage)
-#      print command1
+#      shellCommand(command1,errorMessage)
+      print command1
     command2 = cmdBase
 
     for obj in objFileList:
@@ -81,13 +86,14 @@ def main(argv):
     exe =  'main_' + ifdef
     command2= command2 + ' -o ' + exe
     errorMessage=' Linking failed for ' + exe
-    shellCommand(command2,errorMessage)
+#    shellCommand(command2,errorMessage)
     print command2
     
     print "***Running  " + exe + " ***"
     command3='./' + exe
     errorMessage=' Failed to run ' + exe
-
+#    shellCommand(command3,,errorMessage)
+    print command3
 
 if __name__ == "__main__":
    main(sys.argv[1:])
