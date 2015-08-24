21bb5d8d433aee7098bf4af73a21e4938b61ce17
Srinath Vadlamani <srinathv@ucar.edu>
2015-08-24 16:03:50 -0500

    added batch script and profile dirs

--- tau/symmetric/sandy-knc/mm/tauRun.matmult.sh
+++ tau/symmetric/sandy-knc/mm/tauRun.matmult.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 export TAU_MAKEFILE=/work/02463/srinathv/tau-install/useSysGnu/x86_64/lib/Makefile.tau-intel15-icpc-papi-mpi-pdt
+export TAU_PROFILE_DIR=/work/02463/srinathv/improvehpc/tau/symmetric/sandy-knc/mm/E5_profiles
 ./matmult
