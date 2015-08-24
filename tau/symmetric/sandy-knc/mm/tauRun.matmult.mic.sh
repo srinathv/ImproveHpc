6034fae95f77c360c9d392217d4ed446cd76e90b
Srinath Vadlamani <srinathv@ucar.edu>
2015-08-24 14:38:42 -0500

    need these scripts to run in symmetric

---
+++ tau/symmetric/sandy-knc/mm/tauRun.matmult.mic.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+export TAU_MAKEFILE=/work/02463/srinathv/tau-install/useSysGnu/mic_linux/lib/Makefile.tau-intel15-icpc-papi-mpi-pdt
+./matmult.mic
---
+++ tau/symmetric/sandy-knc/mm/tauRun.matmult.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+export TAU_MAKEFILE=/work/02463/srinathv/tau-install/useSysGnu/x86_64/lib/Makefile.tau-intel15-icpc-papi-mpi-pdt
+./matmult
