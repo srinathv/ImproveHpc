e6b744fe52e8bf2ad5e6d43a62283b6e395496e3
Srinath Vadlamani <srinathv@ucar.edu>
2015-08-24 16:26:14 -0500

    Runs on Stampede

--- tau/symmetric/sandy-knc/mm/tauRun.matmult.mic.sh
+++ tau/symmetric/sandy-knc/mm/tauRun.matmult.mic.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 export TAU_MAKEFILE=/work/02463/srinathv/tau-install/useSysGnu/mic_linux/lib/Makefile.tau-intel15-icpc-papi-mpi-pdt
-export TAU_PROFILE_DIR=/work/02463/srinathv/improvehpc/tau/symmetric/sandy-knc/mm/KNC_profiles
+export PROFILEDIR=/work/02463/srinathv/improvehpc/tau/symmetric/sandy-knc/mm/KNC_profiles
 ./matmult.mic
