adeb5b555e7d55cac0d2bc60b024705a30922ab5
Srinath Vadlamani <srinathv@ucar.edu>
2016-12-05 22:05:58 -0600

    ibrun works in interactive mode on stampede login-knl1

--- threading/tbb/tau_mm/run.stampede.sh
+++ threading/tbb/tau_mm/run.stampede.sh
@@ -1,3 +1,4 @@
 source tau_source.stampede.this
 #tau_exec -T knl,icpc,papi,mpi,tbb ./matmult_tbb_tau.exe
-mpirun -n 2 tau_exec -T knl,icpc,papi,mpi,tbb ./matmult_tbb_mpi_tau.exe
+#mpirun -n 2 tau_exec -T knl,icpc,papi,mpi,tbb ./matmult_tbb_mpi_tau.exe
+ibrun -n 2 -o 0 tau_exec -T knl,icpc,papi,mpi,tbb ./matmult_tbb_mpi_tau.exe
